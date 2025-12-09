76a44df3852a957a13fa396e171b4fc028b57847
Ilya Zazvonov <izazvonov@rvision.ru>
2025-12-09 22:22:50 +0400

    feat: delete navigation initialization and add migrations, fix tests

--- CarRental.Domain/DataSeed/DomainTestsData.cs
+++ CarRental.Domain/DataSeed/DomainTestsData.cs
@@ -50,34 +50,34 @@ public class CarRentalDataSeed
 
         ModelGenerations =
         [
-            new ModelGeneration { Id = 1, Year = 2020, EngineVolume = 2.5f, ModelId = CarModels[0].Id, Model = CarModels[0], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 2000 },
-            new ModelGeneration { Id = 2, Year = 2022, EngineVolume = 2.0f, ModelId = CarModels[0].Id, Model = CarModels[0], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 2200 },
-            new ModelGeneration { Id = 3, Year = 2021, EngineVolume = 3.0f, ModelId = CarModels[1].Id, Model = CarModels[1], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 3500 },
-            new ModelGeneration { Id = 4, Year = 2019, EngineVolume = 1.6f, ModelId = CarModels[2].Id, Model = CarModels[2], TransmissionType = CarTransmissionType.Manual, PricePerHour = 900 },
-            new ModelGeneration { Id = 5, Year = 2020, EngineVolume = 1.6f, ModelId = CarModels[3].Id, Model = CarModels[3], TransmissionType = CarTransmissionType.AutomatedManual, PricePerHour = 1100 },
-            new ModelGeneration { Id = 6, Year = 2022, EngineVolume = 3.0f, ModelId = CarModels[4].Id, Model = CarModels[4], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 5000 },
-            new ModelGeneration { Id = 7, Year = 2021, EngineVolume = 1.4f, ModelId = CarModels[5].Id, Model = CarModels[5], TransmissionType = CarTransmissionType.Manual, PricePerHour = 950 },
-            new ModelGeneration { Id = 8, Year = 2023, EngineVolume = 2.0f, ModelId = CarModels[6].Id, Model = CarModels[6], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 1800 },
-            new ModelGeneration { Id = 9, Year = 2022, EngineVolume = 1.8f, ModelId = CarModels[7].Id, Model = CarModels[7], TransmissionType = CarTransmissionType.Automatic, PricePerHour = 1700 },
-            new ModelGeneration { Id = 10, Year = 2020, EngineVolume = 1.4f, ModelId = CarModels[5].Id, Model = CarModels[5], TransmissionType = CarTransmissionType.ContinuouslyVariable, PricePerHour = 1000 }
+            new ModelGeneration { Id = 1, Year = 2020, EngineVolume = 2.5f, ModelId = CarModels[0].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 2000 },
+            new ModelGeneration { Id = 2, Year = 2022, EngineVolume = 2.0f, ModelId = CarModels[0].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 2200 },
+            new ModelGeneration { Id = 3, Year = 2021, EngineVolume = 3.0f, ModelId = CarModels[1].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 3500 },
+            new ModelGeneration { Id = 4, Year = 2019, EngineVolume = 1.6f, ModelId = CarModels[2].Id, TransmissionType = CarTransmissionType.Manual, PricePerHour = 900 },
+            new ModelGeneration { Id = 5, Year = 2020, EngineVolume = 1.6f, ModelId = CarModels[3].Id, TransmissionType = CarTransmissionType.AutomatedManual, PricePerHour = 1100 },
+            new ModelGeneration { Id = 6, Year = 2022, EngineVolume = 3.0f, ModelId = CarModels[4].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 5000 },
+            new ModelGeneration { Id = 7, Year = 2021, EngineVolume = 1.4f, ModelId = CarModels[5].Id, TransmissionType = CarTransmissionType.Manual, PricePerHour = 950 },
+            new ModelGeneration { Id = 8, Year = 2023, EngineVolume = 2.0f, ModelId = CarModels[6].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 1800 },
+            new ModelGeneration { Id = 9, Year = 2022, EngineVolume = 1.8f, ModelId = CarModels[7].Id, TransmissionType = CarTransmissionType.Automatic, PricePerHour = 1700 },
+            new ModelGeneration { Id = 10, Year = 2020, EngineVolume = 1.4f, ModelId = CarModels[5].Id, TransmissionType = CarTransmissionType.ContinuouslyVariable, PricePerHour = 1000 }
         ];
 
         Cars =
         [
-            new Car { Id = 1, GenerationId = ModelGenerations[0].Id, Generation = ModelGenerations[0], LicensePlate = "A123BC777", Color = Color.Black },
-            new Car { Id = 2, GenerationId = ModelGenerations[0].Id, Generation = ModelGenerations[0], LicensePlate = "B456OP777", Color = Color.White },
-            new Car { Id = 3, GenerationId = ModelGenerations[1].Id, Generation = ModelGenerations[1], LicensePlate = "E789TT777", Color = Color.Silver },
-            new Car { Id = 4, GenerationId = ModelGenerations[2].Id, Generation = ModelGenerations[2], LicensePlate = "K321MM777", Color = Color.Blue },
-            new Car { Id = 5, GenerationId = ModelGenerations[2].Id, Generation = ModelGenerations[2], LicensePlate = "M654HH777", Color = Color.Black },
-            new Car { Id = 6, GenerationId = ModelGenerations[3].Id, Generation = ModelGenerations[3], LicensePlate = "O987PP777", Color = Color.Red },
-            new Car { Id = 7, GenerationId = ModelGenerations[3].Id, Generation = ModelGenerations[3], LicensePlate = "P159CC777", Color = Color.White },
-            new Car { Id = 8, GenerationId = ModelGenerations[4].Id, Generation = ModelGenerations[4], LicensePlate = "C753YY777", Color = Color.Gray },
-            new Car { Id = 9, GenerationId = ModelGenerations[5].Id, Generation = ModelGenerations[5], LicensePlate = "T456FF777", Color = Color.Black },
-            new Car { Id = 10, GenerationId = ModelGenerations[5].Id, Generation = ModelGenerations[5], LicensePlate = "Y789XX777", Color = Color.White },
-            new Car { Id = 11, GenerationId = ModelGenerations[6].Id, Generation = ModelGenerations[6], LicensePlate = "X123KK777", Color = Color.Blue },
-            new Car { Id = 12, GenerationId = ModelGenerations[7].Id, Generation = ModelGenerations[7], LicensePlate = "C456LL777", Color = Color.Green },
-            new Car { Id = 13, GenerationId = ModelGenerations[8].Id, Generation = ModelGenerations[8], LicensePlate = "Z789MM777", Color = Color.Silver },
-            new Car { Id = 14, GenerationId = ModelGenerations[9].Id, Generation = ModelGenerations[9], LicensePlate = "H321HH777", Color = Color.Red }
+            new Car { Id = 1, GenerationId = ModelGenerations[0].Id,LicensePlate = "A123BC777", Color = Color.Black },
+            new Car { Id = 2, GenerationId = ModelGenerations[0].Id, LicensePlate = "B456OP777", Color = Color.White },
+            new Car { Id = 3, GenerationId = ModelGenerations[1].Id, LicensePlate = "E789TT777", Color = Color.Silver },
+            new Car { Id = 4, GenerationId = ModelGenerations[2].Id, LicensePlate = "K321MM777", Color = Color.Blue },
+            new Car { Id = 5, GenerationId = ModelGenerations[2].Id, LicensePlate = "M654HH777", Color = Color.Black },
+            new Car { Id = 6, GenerationId = ModelGenerations[3].Id, LicensePlate = "O987PP777", Color = Color.Red },
+            new Car { Id = 7, GenerationId = ModelGenerations[3].Id, LicensePlate = "P159CC777", Color = Color.White },
+            new Car { Id = 8, GenerationId = ModelGenerations[4].Id, LicensePlate = "C753YY777", Color = Color.Gray },
+            new Car { Id = 9, GenerationId = ModelGenerations[5].Id, LicensePlate = "T456FF777", Color = Color.Black },
+            new Car { Id = 10, GenerationId = ModelGenerations[5].Id, LicensePlate = "Y789XX777", Color = Color.White },
+            new Car { Id = 11, GenerationId = ModelGenerations[6].Id, LicensePlate = "X123KK777", Color = Color.Blue },
+            new Car { Id = 12, GenerationId = ModelGenerations[7].Id, LicensePlate = "C456LL777", Color = Color.Green },
+            new Car { Id = 13, GenerationId = ModelGenerations[8].Id, LicensePlate = "Z789MM777", Color = Color.Silver },
+            new Car { Id = 14, GenerationId = ModelGenerations[9].Id, LicensePlate = "H321HH777", Color = Color.Red }
         ];
 
         Clients =
@@ -97,46 +97,46 @@ public class CarRentalDataSeed
         RentalLogs =
         [
             // Toyota Camry (most popular)
-            new RentalLog { Id = 1, CarId = Cars[0].Id, Car = Cars[0], ClientId = Clients[0].Id, Client = Clients[0], RentStartDate = new DateTime(2024, 1, 10, 10, 0, 0), Duration = 24 },
-            new RentalLog { Id = 2, CarId = Cars[0].Id, Car = Cars[0], ClientId = Clients[1].Id, Client = Clients[1], RentStartDate = new DateTime(2024, 1, 15, 14, 0, 0), Duration = 48 },
-            new RentalLog { Id = 3, CarId = Cars[0].Id, Car = Cars[0], ClientId = Clients[2].Id, Client = Clients[2], RentStartDate = new DateTime(2024, 2, 1, 9, 0, 0), Duration = 12 },
-            new RentalLog { Id = 4, CarId = Cars[1].Id, Car = Cars[1], ClientId = Clients[3].Id, Client = Clients[3], RentStartDate = new DateTime(2024, 1, 20, 16, 0, 0), Duration = 72 },
-            new RentalLog { Id = 5, CarId = Cars[1].Id, Car = Cars[1], ClientId = Clients[0].Id, Client = Clients[0], RentStartDate = new DateTime(2024, 2, 5, 11, 0, 0), Duration = 24 },
-            new RentalLog { Id = 6, CarId = Cars[2].Id, Car = Cars[2], ClientId = Clients[4].Id, Client = Clients[4], RentStartDate = new DateTime(2024, 1, 25, 8, 0, 0), Duration = 36 },
+            new RentalLog { Id = 1, CarId = Cars[0].Id, ClientId = Clients[0].Id, RentStartDate = new DateTime(2024, 1, 10, 10, 0, 0), Duration = 24 },
+            new RentalLog { Id = 2, CarId = Cars[0].Id, ClientId = Clients[1].Id, RentStartDate = new DateTime(2024, 1, 15, 14, 0, 0), Duration = 48 },
+            new RentalLog { Id = 3, CarId = Cars[0].Id, ClientId = Clients[2].Id, RentStartDate = new DateTime(2024, 2, 1, 9, 0, 0), Duration = 12 },
+            new RentalLog { Id = 4, CarId = Cars[1].Id, ClientId = Clients[3].Id, RentStartDate = new DateTime(2024, 1, 20, 16, 0, 0), Duration = 72 },
+            new RentalLog { Id = 5, CarId = Cars[1].Id, ClientId = Clients[0].Id, RentStartDate = new DateTime(2024, 2, 5, 11, 0, 0), Duration = 24 },
+            new RentalLog { Id = 6, CarId = Cars[2].Id, ClientId = Clients[4].Id, RentStartDate = new DateTime(2024, 1, 25, 8, 0, 0), Duration = 36 },
 
             // BMW X5
-            new RentalLog { Id = 7, CarId = Cars[3].Id, Car = Cars[3], ClientId = Clients[5].Id, Client = Clients[5], RentStartDate = new DateTime(2024, 2, 10, 13, 0, 0), Duration = 24 },
-            new RentalLog { Id = 8, CarId = Cars[3].Id, Car = Cars[3], ClientId = Clients[1].Id, Client = Clients[1], RentStartDate = new DateTime(2024, 2, 12, 10, 0, 0), Duration = 48 },
-            new RentalLog { Id = 9, CarId = Cars[4].Id, Car = Cars[4], ClientId = Clients[6].Id, Client = Clients[6], RentStartDate = new DateTime(2024, 1, 30, 15, 0, 0), Duration = 12 },
+            new RentalLog { Id = 7, CarId = Cars[3].Id, ClientId = Clients[5].Id, RentStartDate = new DateTime(2024, 2, 10, 13, 0, 0), Duration = 24 },
+            new RentalLog { Id = 8, CarId = Cars[3].Id, ClientId = Clients[1].Id, RentStartDate = new DateTime(2024, 2, 12, 10, 0, 0), Duration = 48 },
+            new RentalLog { Id = 9, CarId = Cars[4].Id, ClientId = Clients[6].Id, RentStartDate = new DateTime(2024, 1, 30, 15, 0, 0), Duration = 12 },
 
             // Lada Vesta
-            new RentalLog { Id = 10, CarId = Cars[5].Id, Car = Cars[5], ClientId = Clients[7].Id, Client = Clients[7], RentStartDate = new DateTime(2024, 2, 3, 14, 0, 0), Duration = 60 },
-            new RentalLog { Id = 11, CarId = Cars[6].Id, Car = Cars[6], ClientId = Clients[0].Id, Client = Clients[0], RentStartDate = new DateTime(2024, 2, 7, 16, 0, 0), Duration = 18 },
+            new RentalLog { Id = 10, CarId = Cars[5].Id, ClientId = Clients[7].Id, RentStartDate = new DateTime(2024, 2, 3, 14, 0, 0), Duration = 60 },
+            new RentalLog { Id = 11, CarId = Cars[6].Id, ClientId = Clients[0].Id, RentStartDate = new DateTime(2024, 2, 7, 16, 0, 0), Duration = 18 },
 
             // Kia Rio
-            new RentalLog { Id = 12, CarId = Cars[7].Id, Car = Cars[7], ClientId = Clients[3].Id, Client = Clients[3], RentStartDate = new DateTime(2024, 2, 9, 11, 0, 0), Duration = 24 },
+            new RentalLog { Id = 12, CarId = Cars[7].Id, ClientId = Clients[3].Id, RentStartDate = new DateTime(2024, 2, 9, 11, 0, 0), Duration = 24 },
 
             // Mercedes S-Class
-            new RentalLog { Id = 13, CarId = Cars[8].Id, Car = Cars[8], ClientId = Clients[4].Id, Client = Clients[4], RentStartDate = new DateTime(2024, 2, 11, 10, 0, 0), Duration = 36 },
-            new RentalLog { Id = 14, CarId = Cars[9].Id, Car = Cars[9], ClientId = Clients[5].Id, Client = Clients[5], RentStartDate = new DateTime(2024, 2, 13, 12, 0, 0), Duration = 48 },
+            new RentalLog { Id = 13, CarId = Cars[8].Id, ClientId = Clients[4].Id, RentStartDate = new DateTime(2024, 2, 11, 10, 0, 0), Duration = 36 },
+            new RentalLog { Id = 14, CarId = Cars[9].Id, ClientId = Clients[5].Id, RentStartDate = new DateTime(2024, 2, 13, 12, 0, 0), Duration = 48 },
 
             // Volkswagen Polo
-            new RentalLog { Id = 15, CarId = Cars[10].Id, Car = Cars[10], ClientId = Clients[6].Id, Client = Clients[6], RentStartDate = new DateTime(2024, 2, 14, 8, 0, 0), Duration = 24 },
-            new RentalLog { Id = 16, CarId = Cars[13].Id, Car = Cars[13], ClientId = Clients[7].Id, Client = Clients[7], RentStartDate = new DateTime(2024, 2, 15, 14, 0, 0), Duration = 12 },
+            new RentalLog { Id = 15, CarId = Cars[10].Id, ClientId = Clients[6].Id, RentStartDate = new DateTime(2024, 2, 14, 8, 0, 0), Duration = 24 },
+            new RentalLog { Id = 16, CarId = Cars[13].Id, ClientId = Clients[7].Id, RentStartDate = new DateTime(2024, 2, 15, 14, 0, 0), Duration = 12 },
 
             // Hyundai Creta
-            new RentalLog { Id = 17, CarId = Cars[11].Id, Car = Cars[11], ClientId = Clients[8].Id, Client = Clients[8], RentStartDate = new DateTime(2024, 2, 16, 9, 0, 0), Duration = 48 },
+            new RentalLog { Id = 17, CarId = Cars[11].Id, ClientId = Clients[8].Id, RentStartDate = new DateTime(2024, 2, 16, 9, 0, 0), Duration = 48 },
 
             // Skoda Octavia
-            new RentalLog { Id = 18, CarId = Cars[12].Id, Car = Cars[12], ClientId = Clients[9].Id, Client = Clients[9], RentStartDate = new DateTime(2024, 2, 17, 16, 0, 0), Duration = 24 },
+            new RentalLog { Id = 18, CarId = Cars[12].Id, ClientId = Clients[9].Id, RentStartDate = new DateTime(2024, 2, 17, 16, 0, 0), Duration = 24 },
 
             // Other rents for statistics
-            new RentalLog { Id = 19, CarId = Cars[0].Id, Car = Cars[0], ClientId = Clients[2].Id, Client = Clients[2], RentStartDate = new DateTime(2024, 2, 18, 11, 0, 0), Duration = 36 },
-            new RentalLog { Id = 20, CarId = Cars[1].Id, Car = Cars[1], ClientId = Clients[4].Id, Client = Clients[4], RentStartDate = new DateTime(2024, 2, 19, 13, 0, 0), Duration = 24 },
-            new RentalLog { Id = 21, CarId = Cars[3].Id, Car = Cars[3], ClientId = Clients[6].Id, Client = Clients[6], RentStartDate = new DateTime(2024, 2, 20, 10, 0, 0), Duration = 18 },
-            new RentalLog { Id = 22, CarId = Cars[5].Id, Car = Cars[5], ClientId = Clients[8].Id, Client = Clients[8], RentStartDate = new DateTime(2024, 2, 21, 15, 0, 0), Duration = 72 },
-            new RentalLog { Id = 23, CarId = Cars[8].Id, Car = Cars[8], ClientId = Clients[0].Id, Client = Clients[0], RentStartDate = new DateTime(2024, 2, 22, 12, 0, 0), Duration = 24 },
-            new RentalLog { Id = 24, CarId = Cars[10].Id, Car = Cars[10], ClientId = Clients[1].Id, Client = Clients[1], RentStartDate = new DateTime(2024, 2, 23, 9, 0, 0), Duration = 36 }
+            new RentalLog { Id = 19, CarId = Cars[0].Id, ClientId = Clients[2].Id, RentStartDate = new DateTime(2024, 2, 18, 11, 0, 0), Duration = 36 },
+            new RentalLog { Id = 20, CarId = Cars[1].Id, ClientId = Clients[4].Id, RentStartDate = new DateTime(2024, 2, 19, 13, 0, 0), Duration = 24 },
+            new RentalLog { Id = 21, CarId = Cars[3].Id, ClientId = Clients[6].Id, RentStartDate = new DateTime(2024, 2, 20, 10, 0, 0), Duration = 18 },
+            new RentalLog { Id = 22, CarId = Cars[5].Id, ClientId = Clients[8].Id, RentStartDate = new DateTime(2024, 2, 21, 15, 0, 0), Duration = 72 },
+            new RentalLog { Id = 23, CarId = Cars[8].Id, ClientId = Clients[0].Id, RentStartDate = new DateTime(2024, 2, 22, 12, 0, 0), Duration = 24 },
+            new RentalLog { Id = 24, CarId = Cars[10].Id, ClientId = Clients[1].Id, RentStartDate = new DateTime(2024, 2, 23, 9, 0, 0), Duration = 36 }
         ];
     }
 }
--- CarRental.Tests/DomainTests.cs
+++ CarRental.Tests/DomainTests.cs
@@ -8,28 +8,35 @@ namespace CarRental.Tests;
 public class CarRenatalDomainTests(CarRentalDataSeed fixture) : IClassFixture<CarRentalDataSeed>
 {
     /// <summary>
-    /// Retrieves all clients who have rented cars of a specified model, 
+    /// Retrieves all clients who have rented cars of a specified model,
     /// ordered alphabetically by last name, first name, and patronymic
     /// </summary>
     [Fact]
     public void GetClientsRentedModel()
     {
         var target = fixture.CarModels[1];
-
         var expectedClientsId = new[] { 2, 6, 7 };
 
+        var carById = fixture.Cars.ToDictionary(c => c.Id);
+        var genById = fixture.ModelGenerations.ToDictionary(g => g.Id);
+        var clientById = fixture.Clients.ToDictionary(c => c.Id);
+
         var actual = fixture.RentalLogs
-            .Where(r => r.Car!.Generation!.Model!.Name == target.Name)
-            .Select(r => r.Client)
-            .Distinct()
-            .OrderBy(c => c!.LastName)
-            .ThenBy(c => c!.FirstName)
-            .ThenBy(c => c!.Patronymic)
-            .ToList()
-            .Select(c => c!.Id);
+            .Where(r =>
+            {
+                var car = carById[r.CarId];
+                var gen = genById[car.GenerationId];
+                return gen.ModelId == target.Id;
+            })
+            .Select(r => clientById[r.ClientId])
+            .DistinctBy(c => c.Id)
+            .OrderBy(c => c.LastName)
+            .ThenBy(c => c.FirstName)
+            .ThenBy(c => c.Patronymic)
+            .Select(c => c.Id);
+
         Assert.Equal(expectedClientsId, actual);
     }
-
 
     /// <summary>
     /// Retrieves all cars that are currently rented
@@ -38,12 +45,12 @@ public class CarRenatalDomainTests(CarRentalDataSeed fixture) : IClassFixture<Ca
     public void GetCarsInRent()
     {
         var currentTime = new DateTime(2024, 2, 19, 14, 0, 0);
-
         var expectedCarsId = new[] { 1, 2 };
 
         var actual = fixture.RentalLogs
-            .Where(r => r.RentStartDate <= currentTime && currentTime <= r.RentStartDate.AddHours((double)r.Duration))
+            .Where(r => r.RentStartDate <= currentTime && currentTime <= r.RentStartDate.AddHours(r.Duration))
             .Select(r => r.CarId);
+
         Assert.Equal(expectedCarsId, actual);
     }
 
@@ -56,12 +63,14 @@ public class CarRenatalDomainTests(CarRentalDataSeed fixture) : IClassFixture<Ca
         var expectedCarsId = new[] { 1, 2, 4, 6, 9 };
 
         var actual = fixture.RentalLogs
-            .GroupBy(log => log.Car)
-            .Select(g => new { Car = g.Key, Count = g.Count() })
+            .GroupBy(log => log.CarId)
+            .Select(g => new { CarId = g.Key, Count = g.Count() })
             .OrderByDescending(x => x.Count)
+            .ThenBy(x => x.CarId)
             .Take(5)
-            .Select(x => x.Car!.Id)
+            .Select(x => x.CarId)
             .ToList();
+
         Assert.Equal(expectedCarsId, actual);
     }
 
@@ -74,27 +83,23 @@ public class CarRenatalDomainTests(CarRentalDataSeed fixture) : IClassFixture<Ca
         var allCars = fixture.Cars
             .OrderBy(c => c.Id)
             .ToList();
+
         var expected = new[] { 4, 3, 1, 3, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1 };
 
-        var carsLen = allCars.Count;
-
         var expectedResult = new Dictionary<int, int>();
-        for (var i = 0; i < carsLen; i++)
-        {
+        for (var i = 0; i < allCars.Count; i++)
             expectedResult[i + 1] = expected[i];
-        }
 
         var actual = fixture.RentalLogs
-            .GroupBy(r => r.Car)
-            .Select(g => new { Car = g.Key, Count = g.Count() })
-            .OrderBy(c => c.Car!.Id)
-            .ToDictionary(g => g.Car!.Id, g => g.Count);
+            .GroupBy(r => r.CarId)
+            .OrderBy(g => g.Key)
+            .ToDictionary(g => g.Key, g => g.Count());
 
         Assert.Equal(expectedResult, actual);
     }
 
     /// <summary>
-    /// Retrieves the top 5 clients with the highest total rental amount, 
+    /// Retrieves the top 5 clients with the highest total rental amount,
     /// calculated as the sum of (duration × price per hour) across all their rentals
     /// </summary>
     [Fact]
@@ -102,18 +107,27 @@ public class CarRenatalDomainTests(CarRentalDataSeed fixture) : IClassFixture<Ca
     {
         var expectedClientsId = new[] { 6, 5, 2, 1, 4 };
 
-        var actual = fixture.RentalLogs
-        .GroupBy(r => r.Client)
-        .Select(g => new
-        {
-            Client = g.Key,
-            TotalAmount = g.Sum(r => (decimal)r.Duration * r.Car!.Generation!.PricePerHour)
-        })
-        .OrderByDescending(x => x.TotalAmount)
-        .Take(5)
-        .Select(c => c.Client!.Id)
-        .ToList();
+        var carById = fixture.Cars.ToDictionary(c => c.Id);
+        var genById = fixture.ModelGenerations.ToDictionary(g => g.Id);
 
-        Assert.Equal(actual, expectedClientsId);
+        var actual = fixture.RentalLogs
+            .GroupBy(r => r.ClientId)
+            .Select(g => new
+            {
+                ClientId = g.Key,
+                TotalAmount = g.Sum(r =>
+                {
+                    var car = carById[r.CarId];
+                    var gen = genById[car.GenerationId];
+                    return (decimal)r.Duration * gen.PricePerHour;
+                })
+            })
+            .OrderByDescending(x => x.TotalAmount)
+            .ThenBy(x => x.ClientId)
+            .Take(5)
+            .Select(x => x.ClientId)
+            .ToList();
+
+        Assert.Equal(expectedClientsId, actual);
     }
 }
